f7b08deb02ac092b2970d65a57314b244afdadab
RzayevUmid0023 <umid.official.main@gmail.com>
2023-10-22 18:54:20 +0400

    Version 1.3
    Responsivv sign-up , sign-in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,11 +10,13 @@ function LoginPage({ showLogin, toggleShowLogin }) {
   const registerBtn = useRef(null);
   const loginBtn = useRef(null);
 
-  const handleRegister = () => {
+  const handleRegister = (event) => {
+    event.preventDefault();
     container.current.classList.add("active");
   };
 
-  const handleLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
     container.current.classList.remove("active");
   };
 
@@ -24,56 +26,63 @@ function LoginPage({ showLogin, toggleShowLogin }) {
            
             <div className="login-container" ref={container}>
 
-                <div className="form-container sign-up">
-            <div className='exit_button'><button onClick={toggleShowLogin}><BiExit className='exit-icon'/></button></div>
+                  <div className="form-container sign-up ">
+                      <div className='exit_button'><button onClick={toggleShowLogin}><BiExit className='exit-icon'/></button></div>
 
-                <form>
-                    <h1>Hesap Oluştur</h1>
-                    <div className="social-icons">
-                     
-                    <a href="#" className="icon"><FaGoogle /></a>
-                    <a href="#" className="icon"><FaFacebook /></a>
-                    <a href="#" className="icon"><FaTwitter /> </a>
-                    </div>
-                    <span>Veya kayıt için E-posta adresinizi kullanın</span>
-                    <input type="text" placeholder="İsim" />
-                    <input type="email" placeholder="E-Posta" />
-                    <input type="password" placeholder="Şifre" />
-                    <button>Kaydol</button>
-                </form>
-                </div>
-                <div className="form-container sign-in">
-            <div className='exit_button'><button onClick={toggleShowLogin}><BiExit className='exit-icon'/></button></div>
+                      <form className='sign-up-form'>
+                          <h1>Hesap Oluştur</h1>
+                          <div className="social-icons">
+                          
+                          <a href="#" className="icon"><FaGoogle /></a>
+                          <a href="#" className="icon"><FaFacebook /></a>
+                          <a href="#" className="icon"><FaTwitter /> </a>
+                          </div>  
+                          <span>Veya kayıt için E-posta adresinizi kullanın</span>
+                          <input type="text" placeholder="İsim" />
+                          <input type="email" placeholder="E-Posta" />
+                          <input type="password" placeholder="Şifre" />
+                          <button>Kaydol</button>
+                          <button className="hidden-800" ref={loginBtn} onClick={handleLogin}>Giriş Yapın</button>
 
-                <form>
-                    <img src={Logo }/>
-                    <div className="social-icons">
-                    {/* <i> etiketlerini <ICON_NAME /> komponentleriyle değiştir */}
-                    <a href="#" className="icon"><FaGoogle /></a>
-                    <a href="#" className="icon"><FaFacebook /></a>
-                    <a href="#" className="icon"><FaTwitter/> </a>
+                      </form>
+                  </div>
+
+                  <div className="form-container sign-in">
+                      <div className='exit_button'><button onClick={toggleShowLogin}><BiExit className='exit-icon'/></button></div>
+
+                      <form className='sign-in-form'>
+                          <img src={Logo }/>
+                          <div className="social-icons">
+                          <a href="#" className="icon"><FaGoogle /></a>
+                          <a href="#" className="icon"><FaFacebook /></a>
+                          <a href="#" className="icon"><FaTwitter/> </a>
+                          </div>
+                          <span>Veya E-postanız ile giriş yapın</span>
+                          <input type="email" placeholder="E-Posta" />
+                          <input type="password" placeholder="Şifre" />
+                          <a href="#">Şifremi Unuttum</a>
+                          <button>Giriş Yap</button>
+                          <button className="hidden-800" ref={registerBtn} onClick={handleRegister}>Kaydol</button>
+
+                          
+                      </form>
+                  </div>  
+
+                  <div className="toggle-container">
+                  <div className="toggle">
+                      <div className="toggle-panel toggle-left">
+                          <h1>Tekrar Hoş Geldiniz!</h1>
+                          <p>Kişisel bilgilerinizi girin</p>
+                          <button className="hidden" ref={loginBtn} onClick={handleLogin}>Giriş Yapın</button>
+                          </div>
+                          <div className="toggle-panel toggle-right">
+                          <h1>Merhaba</h1>
+                          <p>Kişisel bilgileriniz ile kaydolun</p>
+                          <button className="hidden" ref={registerBtn} onClick={handleRegister}>Kaydol</button>
+                        </div>
                     </div>
-                    <span>Veya E-postanız ile giriş yapın</span>
-                    <input type="email" placeholder="E-Posta" />
-                    <input type="password" placeholder="Şifre" />
-                    <a href="#">Şifremi Unuttum</a>
-                    <button>Giriş Yap</button>
-                </form>
                 </div>
-                <div className="toggle-container">
-                <div className="toggle">
-                    <div className="toggle-panel toggle-left">
-                    <h1>Tekrar Hoş Geldiniz!</h1>
-                    <p>Kişisel bilgilerinizi girin</p>
-                    <button className="hidden" ref={loginBtn} onClick={handleLogin}>Giriş Yapın</button>
-                    </div>
-                    <div className="toggle-panel toggle-right">
-                    <h1>Merhaba</h1>
-                    <p>Kişisel bilgileriniz ile kaydolun</p>
-                    <button className="hidden" ref={registerBtn} onClick={handleRegister}>Kaydol</button>
-                    </div>
-                </div>
-                </div>
+                
             </div>
       </div>
 
